c7ce5e1dcd7bb338ee23fcefbd88a60f6cfd8b61
Gustavo Koch <gustavok@outlook.com.br>
2024-04-28 11:54:59 -0300

    fix: fixed migration

--- migrations/Version20240316201711.php
+++ migrations/Version20240316201711.php
@@ -14,18 +14,10 @@ final class Version20240316201711 extends AbstractMigration
         $this->addSql('CREATE TABLE pets (id INT AUTO_INCREMENT NOT NULL, specie_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, breed VARCHAR(255) NOT NULL, color VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, currently_vaccinated TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_8638EA3FD5436AB7 (specie_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE species (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE pets ADD CONSTRAINT FK_8638EA3FD5436AB7 FOREIGN KEY (specie_id) REFERENCES species (id)');
-        $this->addSql('ALTER TABLE animals DROP FOREIGN KEY FK_966C69DDFE54D947');
-        $this->addSql('DROP TABLE animals');
-        $this->addSql('DROP TABLE `groups`');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE animals (id INT AUTO_INCREMENT NOT NULL, group_id INT DEFAULT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, breed VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, color VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, date_of_birth DATE NOT NULL, currently_vaccinated TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_966C69DDFE54D947 (group_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE `groups` (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, description LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE animals ADD CONSTRAINT FK_966C69DDFE54D947 FOREIGN KEY (group_id) REFERENCES `groups` (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE pets DROP FOREIGN KEY FK_8638EA3FD5436AB7');
-        $this->addSql('DROP TABLE pets');
-        $this->addSql('DROP TABLE species');
+
     }
 }
